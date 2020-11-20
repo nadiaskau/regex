aed14a3c305281d402c2a71625a8491ab59919ff
nadiaskau <nadiaskau@hotmail.com>
2020-11-20 13:17:36 +0100

    Update regex0

--- regex.0/regex0.js
+++ regex.0/regex0.js
@@ -1,19 +1,20 @@
 
-let testUrl = "http://dksxit.eu/webdev/=site/ch13s06.html";
+let testUrl = "http://dkexit.eu/webdev/=site/ch13s06.html";
 
 function getDomain(url){
 
-let result;
-let match;
+  let result;
+  let match;
 
-if(match = url.match(/^(?:https?:\/\/)?(?:www\.)?([^:\/\n\?\=]+)/im)) //den sidste del: stopper når der kommer et ? eller =
-{
-    debugger;
-    result = match[1];
+  if(match = url.match(/^(?:https?:\/\/)?(?:www\.)?([^\/\n\?\=]+)/)) //den sidste del: stopper når der kommer et ? eller =
+  //+ gør at domænet kommer med, ellers er output bare d
+  {
+      debugger;
+      result = match[0];
 
-}
-console.log(result);
-return result;
+  }
+  console.log(result);
+  return result;
 
 }
 
